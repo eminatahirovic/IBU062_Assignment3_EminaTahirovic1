14560579661872cd8c233d94c49f24ba3e4373d9
eminatahirovic <emina.tahirovic@stu.ibu.edu.ba>
2024-12-17 20:04:15 +0100

    Initial submission of Packet Tracer network.

---
+++ network-setup.md.m
@@ -0,0 +1,49 @@
+network-setup.md
+
+Device name: PC2
+Device model: PC-print
+IP Address 169.254.178.46/16
+Port FastEthernet0
+
+
+Device name: PC0
+Device model: PC-PT 
+IP Address: 169.254.110.155/16
+
+Device name: Laptop0
+Device model: Laptop-PT
+IP Address: 169.254.221.230/16
+
+Device name:Server0
+Device model:Server-PT
+IP Address:169.254.226.52/16
+
+Device name: Server 2 
+Device model: Server-PT
+IP Address:169.254.123.97/16
+
+Device name: Server1
+Device model: Server-PT
+IP Address: 169.254.189.115/16
+
+Device name: PC1
+Device model:PC-PT
+IP Address:169.254.132.158/16
+
+Device name: Switch0
+Device model: 2960 IOS15
+Hostname: Switch 
+
+Device name: Switch1
+Device model: 2960 IOS15
+Hostname: Switch
+
+Device name: Router4
+Device model:ISR4331
+Hostname: Router
+
+
+In this assignment I did not use the CLI commands 
+because I set up DHCP manually, therefore there was 
+not anything I could have documented besides the content
+ written above. 
